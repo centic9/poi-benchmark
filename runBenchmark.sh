7c48d1e82b04adc2bd84d4b25253db78b52946b8
POI Benchmark User <centic@apache.org>
2018-06-25 14:58:05 +0200

    Also write build-output to log-file as the email-server sometimes refuses to send too long logs

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -6,6 +6,6 @@ git fetch && \
 git rebase origin/master && \
 rm -rf build && \
 ./gradlew clean && \
-./gradlew jmh publishResults processResults && \
+./gradlew jmh publishResults processResults | tee benchmark.log && \
 git add results && git ci -m "Add daily results" && \
 git push
