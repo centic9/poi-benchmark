fe1acd0f95871f28a4ff4dd86dedf7e61fc40d29
POI Benchmark User <centic@apache.org>
2018-06-27 15:34:31 +0200

    Disable parallel build and Gradle daemon

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -6,6 +6,6 @@ git fetch && \
 git rebase origin/master && \
 rm -rf build && \
 ./gradlew clean && \
-./gradlew jmh publishResults processResults | tee benchmark.log && \
+./gradlew --no-parallel --no-daemon jmh publishResults processResults | tee benchmark.log && \
 git add results && git ci -m "Add daily results" && \
 git push
