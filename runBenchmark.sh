f3824ce840e11230852b7c1404734cd71bf7f8f0
POI Benchmark User <centic@apache.org>
2018-06-29 09:44:02 +0200

    Adjust building for Gradle 4.6

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -6,6 +6,7 @@ git fetch && \
 git rebase origin/master && \
 rm -rf build && \
 ./gradlew clean && \
-./gradlew --no-parallel --no-daemon jmh publishResults processResults | tee benchmark.log && \
+./gradlew --no-parallel --no-daemon jmh | tee benchmark.log | tee benchmark.log && \
+./gradlew --no-parallel --no-daemon publishResults processResults && \
 git add results && git ci -m "Add daily results" && \
 git push
