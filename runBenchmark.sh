28aa61de686ea245f816e70935cb2a5ddc55fd03
Dominik Stadler <dominik.stadler@gmx.at>
2019-03-25 06:58:35 +0100

    Do not trigger CI on benchmark results

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -8,5 +8,5 @@ rm -rf build >> benchmark.log && \
 ./gradlew clean >> benchmark.log && \
 ./gradlew --no-parallel --no-daemon jmh >> benchmark.log && \
 ./gradlew --no-parallel --no-daemon publishResults processResults >> benchmark.log && \
-git add results && git ci -m "Add daily results" >> benchmark.log && \
+git add results && git ci -m "[ci-skip] Add daily results" >> benchmark.log && \
 git push >> benchmark.log
