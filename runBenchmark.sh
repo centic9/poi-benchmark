6bef17ee1f7ab2729975b5f6fd64ea17890e2c2a
Dominik Stadler <dominik.stadler@gmx.at>
2020-08-02 22:25:05 +0200

    Try to use a newer Apache Ant

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -2,11 +2,17 @@
 
 cd `dirname $0`
 
+# use a newer Apache Ant if it is available at this location
+export ANT_HOME=/opt/apache-ant-1.10.8
+export PATH=${ANT_HOME}/bin:${PATH}
+
+export GRADLE_CMD="--console=plain --no-parallel --no-daemon"
+
 git fetch > benchmark.log && \
 git rebase origin/master >> benchmark.log && \
 rm -rf build >> benchmark.log && \
-./gradlew clean >> benchmark.log && \
-./gradlew --no-parallel --no-daemon jmh >> benchmark.log && \
-./gradlew --no-parallel --no-daemon publishResults processResults >> benchmark.log && \
+./gradlew ${GRADLE_CMD} clean >> benchmark.log && \
+./gradlew ${GRADLE_CMD} jmh >> benchmark.log && \
+./gradlew ${GRADLE_CMD} publishResults processResults >> benchmark.log && \
 git add results && git ci -m "[ci skip] Add daily results" >> benchmark.log && \
 git push >> benchmark.log
