0e467c2e45ebb93f4a7138d1843a38b8af6827e0
Dominik Stadler <dominik.stadler@gmx.at>
2022-05-04 17:51:50 +0200

    Remove remains of Apache Ant, also redirect stderr

--- runBenchmark.sh
+++ runBenchmark.sh
@@ -5,43 +5,34 @@ cd `dirname $0`
 # limit memory-usage of Gradle process
 export GRADLE_OPTS="-Xmx64m -Dorg.gradle.jvmargs=-Xmx64m"
 
-# use a newer Apache Ant if it is available at this location
-# make sure to also adjust these values in BaseBenchmark.java
-export ANT_HOME=/opt/apache-ant-1.10.11
-export PATH=${ANT_HOME}/bin:${PATH}
-export ANT_OPTS="-Xmx512m"
-
 export GRADLE_CMD="--console=plain --no-parallel --no-daemon"
 
-echo `which ant`
-ant -version
+echo `which javac` > benchmark.log 2>&1
+javac -version >> benchmark.log 2>&1
 
-echo `which javac`
-javac -version
-
-git fetch > benchmark.log && \
-git rebase origin/master >> benchmark.log && \
-rm -rf build >> benchmark.log && \
-./gradlew ${GRADLE_CMD} clean >> benchmark.log && \
-./gradlew ${GRADLE_CMD} jmhJar >> benchmark.log && \
+git fetch >> benchmark.log 2>&1 && \
+git rebase origin/master >> benchmark.log 2>&1 && \
+rm -rf build >> benchmark.log 2>&1 && \
+./gradlew ${GRADLE_CMD} clean >> benchmark.log 2>&1 && \
+./gradlew ${GRADLE_CMD} jmhJar >> benchmark.log 2>&1 && \
 mkdir -p build/reports/jmh/ && \
 java -Xmx8m -jar build/libs/poi-benchmark-jmh.jar \
   -o build/reports/jmh/human.txt \
   -rf JSON \
   -rff build/reports/jmh/results.json && \
-./gradlew ${GRADLE_CMD} publishResults processResults >> benchmark.log
+./gradlew ${GRADLE_CMD} publishResults processResults >> benchmark.log 2>&1
 RET=$?
 if [ ${RET} -ne 0 ]; then
   echo "Failed to run poi-benchmark"
   exit 1
 fi
 
-grep -Ri tenant results >> benchmark.log
+grep -Ri tenant results >> benchmark.log 2>&1
 RET=$?
 if [ ${RET} -ne 1 ]; then
   echo "Found invalid text 'tenanttoken' in the result-files, not committing changes"
   exit 2
 fi
 
-git add results && git commit -m "[ci skip] Add daily results" >> benchmark.log && \
-git push >> benchmark.log
+git add results && git commit -m "[ci skip] Add daily results" >> benchmark.log 2>&1 && \
+git push >> benchmark.log 2>&1
